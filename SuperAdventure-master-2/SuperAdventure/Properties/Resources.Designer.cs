0cf6d873b728555fe8ec05b0ebfa985dc19e2c46
Pedro <pedrogusmao@ua.pt>
2018-04-16 17:14:44 +0100

    icon

--- SuperAdventure-master-2/SuperAdventure/Properties/Resources.Designer.cs
+++ SuperAdventure-master-2/SuperAdventure/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace SuperAdventure.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap UIbackGround1 {
+            get {
+                object obj = ResourceManager.GetObject("UIbackGround1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
